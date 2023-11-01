4bc4f058a6eb4548eccd94424a09d9f5638f229e
ajsantanav <adriansantana.v@gmail.com>
2023-11-01 13:00:33 -0400

    init

--- 50Days/day 1/script.js
+++ 50Days/day 1/script.js
@@ -1,1 +1,18 @@
- 
+ const panels = document.querySelectorAll('.panel');
+ // variable to accept the class/id of a html/css document
+
+
+ panels.forEach(panel => {
+    panel.addEventListener('click', ()=>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+ })
+ //Panels. checks each panel and when clicked adds the active class/id to that one
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+//this function removes the class/id of active of the previous panel
