43faa04d7e0c3481982c3708e7f3ee870b9b0ece
ajsantanav <adriansantana.v@gmail.com>
2023-12-01 18:53:26 -0500

    init

--- 50Days/Day 5/script.js
+++ 50Days/Day 5/script.js
@@ -8,16 +8,16 @@ let int = setInterval(blurring, 30);
 function blurring() {
     load++;
 
-    if(load > 99)
+    if(load > 199)
     {
         clearInterval(int);
     }
 
     loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0);
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+    loadText.style.opacity = scale(load, 0, 199, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 199, 30, 0)}px)`;
 }
-
+//can be found github
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min)*(out_max-out_min)) / 
     (in_max - in_min) + out_min;
